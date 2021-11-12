6fdcd16e95072ef25d0a0853fc7ae0eb5afd7ca6
Riku Sarlin <riku.sarlin@gmail.com>
2021-11-12 22:58:12 +0200

    Exercise 1.07

--- logOutput/index.js
+++ logOutput/index.js
@@ -1,14 +1,26 @@
+import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 const randomUuidv4 = uuidv4();
+var currentStatus;
 
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res) {
+  res.send(currentStatus);
+});
+
+app.listen(port, function() {
+  console.log(`Server started in port ${port}!`)
+});
 
 const showRandomString = () => {
 
   const now = new Date();
   const timestampNow = now.toISOString();
-  console.log(`${timestampNow}: ${randomUuidv4}`);
-
+  currentStatus = `${timestampNow}: ${randomUuidv4}`;
+  console.log(currentStatus);
   setTimeout(showRandomString, 5000);
 }
 
