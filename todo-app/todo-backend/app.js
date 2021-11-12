5b3af4dfdbd1507f1857c751a4ee4d02c7af8175
Riku Sarlin <riku.sarlin@gmail.com>
2021-11-12 21:41:16 +0200

    Exercise 1.05

--- todo-app/todo-backend/app.js
+++ todo-app/todo-backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, function() {
